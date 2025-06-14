ea1655f184f3fa9f95dffcba7f588b881b086100
avhinaw <avhinaaw@gmail.com>
2025-06-14 23:32:54 +0530

    nav updated

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div className='w-screen px-32 h-28 m-auto flex justify-between shadow-lg'>
-        <div>
-        <img src="favicon.png" alt="logo" />
+    <div className='w-screen px-34 h-20 m-auto flex items-center justify-between shadow-lg'>
+        <div className='flex gap-12 font-bold items-center'>
+        <img className='rounded-xl h-12 object-cover' src="./swiggy_mainLogo.jpg" alt="logo" />
         <h4>Other</h4>
         </div>
 
-        <ul className='capitalize flex gap-4'>
+        <ul className='capitalize flex gap-12 text-lg font-semibold'>
             <li>swiggy corportate</li>
             <li>search</li>
             <li>offers</li>
