9ffb1d76d569621d6cc94759a9f0f8cbc57116ff
avhinaw <avhinaaw@gmail.com>
2025-06-14 23:44:13 +0530

    added dishes and restraunts

--- src/app/components/DishOption.tsx
+++ src/app/components/DishOption.tsx
@@ -4,10 +4,10 @@ import FoodCard from './ui/FoodCard'
 
 export default function DishOption({DishData}: {DishData: object[]}) {
   return (
-    <div className='w-[85%] h-[50vh] m-auto mt-10'>
-        <div className='flex justify-between gap-2 py-5'>
+    <div className='w-[80%] h-[40vh] m-auto mt-5'>
+        <div className='flex justify-between gap-2'>
           <h3 className='font-bold text-xl'>What' on your mind</h3>
-          <div className='flex justify-end gap-2 py-5'>
+          <div className='flex justify-end gap-2'>
           <button
             className='bg-[#02060c26] w-8 h-8 rounded-full flex items-center justify-center text-xl hover:bg-[#02060c40] transition-colors'
             aria-label="Scroll left"
@@ -25,7 +25,7 @@ export default function DishOption({DishData}: {DishData: object[]}) {
 
     <div className="overflow-x-auto flex gap-5">
           {DishData.map((item, index) => (
-              <FoodCard image={item.imageId} title={item.action.text} />
+              <FoodCard image={item.imageId} title={item.action.text} key={index} />
           ))}
       </div>
     </div>
--- src/app/components/LocationRestraunts.tsx
+++ src/app/components/LocationRestraunts.tsx
@@ -7,7 +7,7 @@ export default function LocationRestraunts({restData}: {restData: object[]}) {
   return (
     <div className='grid grid-cols-4 gap-5 w-[80%] m-auto'>
         {restData.map((item, index) => (
-            <RestrauntCard title={item.info.name} rating={item.info.avgRating} location={item.info.areaName} image={item.info.cloudinaryImageId} cuisiness={item.info.cuisines} key={index} />
+            <RestrauntCard title={item.info.name} rating={item.info.avgRating} location={item.info.areaName} image={item.info.cloudinaryImageId} cuisiness={item.info.cuisines} id={item.info.id} key={index} />
         ))}
 
     </div>
